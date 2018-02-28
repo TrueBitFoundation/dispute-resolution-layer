d93e1a1f640ef818d241875fcbf819bc4dba5942
hswick <hdswick@gmail.com>
2018-02-28 16:58:31 -0600

    Setup testing environment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-const DisputeResolutionLayer = artifacts.require("./DisputeResolutionLayer.sol");
+const SimpleAdderGame = artifacts.require("./SimpleAdderGame.sol")
+const TaskManager = artifacts.require("./TaskManager.sol")
 
 module.exports = function(deployer) {
-  deployer.deploy(DisputeResolutionLayer);
-};
+  deployer.deploy(SimpleAdderGame)
+  deployer.deploy(TaskManager)
+}
