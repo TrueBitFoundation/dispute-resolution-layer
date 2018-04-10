1bb4260287c05a013263d0b291d43ddaef59c535
hswick <hdswick@gmail.com>
2018-04-10 14:45:18 -0500

    Created export migration file

---
+++ migrations/4_export.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const SimpleAdderVM = artifacts.require("./SimpleAdderVM.sol")
+const BasicVerificationGame = artifacts.require("./BasicVerificationGame.sol")
+
+module.exports = (deployer, network) => {
+  let exportedContracts = {}
+
+  let contracts = [SimpleAdderVM, BasicVerificationGame]
+
+  contracts.forEach((contract) => {
+
+    exportedContracts[contract.contractName] = {
+      abi: contract.abi,
+      address: contract.address
+    }
+  })
+
+  if (!fs.existsSync(__dirname + "/../export/")){
+    fs.mkdirSync(__dirname + "/../export/")
+  }
+
+  let path = __dirname + "/../export/" + network+ ".json"
+
+  fs.writeFile(path, JSON.stringify(exportedContracts), (e) => {if(e) console.error(e) })
+}
