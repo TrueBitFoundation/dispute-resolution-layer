760ab15e934b01a52652ae80283d0f2d03670bbb
juztamau5 <juztamau5@gmail.com>
2018-05-22 13:08:15 -0700

    Improve documentation

--- contracts/test/SimpleAdderVM.sol
+++ contracts/test/SimpleAdderVM.sol
@@ -4,9 +4,13 @@ import "./SimpleAdderInput.sol";
 import "./SimpleAdderState.sol";
 import "../IComputationLayer.sol";
 
+/**
+ * @title Example VM
+ */
 contract SimpleAdderVM is IComputationLayer {
-
-    //Used directly only to run on chain computation, otherwise use runSteps
+    /**
+     * @notice Implementation of IComputationLayer.runStep()
+     */
     function runStep(bytes32[3] currentState, bytes32 nextInput) external pure returns (bytes32[3] newState) {
         // Get registers
         var (sum, stepNumber) = SimpleAdderState.getRegisters(currentState);
@@ -22,8 +26,11 @@ contract SimpleAdderVM is IComputationLayer {
         newState = SimpleAdderState.getState(nextNumber, sum, stepNumber);
     }
 
-    //Simple list merklization (works like sum)
+    /**
+     * @notice Implementation of IRepeatedGameComputationLayer.merklizeState()
+     */
     function merklizeState(bytes32[3] state) external pure returns (bytes32 merkleRoot) {
+        // Simple list merklization (works like sum)
         for (uint i = 0; i < state.length; i++) {
             if (i == 0) {
                 merkleRoot = state[0];
@@ -33,8 +40,17 @@ contract SimpleAdderVM is IComputationLayer {
         }
     }
 
-    //Used for generating results for query/response
-    //Run offchain
+    /**
+     * @notice Used for generating results for query/response
+     *
+     * @dev Run offchain
+     *
+     * @param program The input
+     * @param numSteps The number of steps to run
+     *
+     * @return state The final state
+     * @return stateHash The hash of the final state
+     */
     function runSteps(bytes32[] program, uint numSteps) external view returns (bytes32[3] state, bytes32 stateHash) {
         for (uint i = 0; i < program.length && i < numSteps; i++) {
             bytes32 nextInstruction = program[i];
